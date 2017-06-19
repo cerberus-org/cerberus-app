b281aecd7baea19f626a3489c6ac18833d344bac
Hillary Arurang <hillarurang@mail.com>
2017-06-19 13:56:28 -0500

    Organized module imports

--- client/app/app.component.spec.ts
+++ client/app/app.component.spec.ts
@@ -9,7 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
-// volunteerService
+// volunteer service
 import { HttpModule } from '@angular/http';
 
 describe('AppComponent', () => {
@@ -22,7 +22,7 @@ describe('AppComponent', () => {
         FormsModule,
         ReactiveFormsModule,
         MaterialModule,
-        // volunteerService
+        // volunteer service
         HttpModule
       ],
       declarations: [
@@ -37,10 +37,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-
+  
   it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+  const fixture = TestBed.createComponent(AppComponent);
+  const app = fixture.debugElement.componentInstance;
+  expect(app.title).toEqual('app');
   }));
 });
