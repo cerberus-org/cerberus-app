cf8605d606b872c723895f7c94239e9683dce9ba
Ted Mader <ted.mader@ge.com>
2017-06-20 10:17:59 -0500

    Define unique compound index for volunteer schema

--- server/models/volunteer.ts
+++ server/models/volunteer.ts
@@ -5,15 +5,17 @@ const volunteerSchema = new mongoose.Schema({
     type: String,
     required: [true, 'First name is required']
   },
-  lastName:  {
+  lastName: {
     type: String,
     required: [true, 'Last name is required']
   },
-  petName:  {
+  petName: {
     type: String,
     required: [true, 'Favorite pet name is required']
   },
-}, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at'} });
+}, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
+
+volunteerSchema.index({ firstName: 1, lastName: 1, petName: 1 }, { unique: true });
 
 const Volunteer = mongoose.model('Volunteer', volunteerSchema);
 
