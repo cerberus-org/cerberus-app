c1947da96b9eed0916602d22dd6096ff9a5d1b09
Ted Mader <tlmader.dev@gmail.com>
2017-06-23 19:20:42 -0500

    Begin creating tests for volunteer controller

---
+++ server/controllers/volunteer.spec.ts
@@ -0,0 +1,31 @@
+describe('VolunteerController', function() {
+
+  describe('Get all', function() {
+
+    it('returns status code 200', function() {
+
+    });
+
+  });
+
+  describe('Count all', function() {
+
+  });
+
+  describe('Insert', function() {
+
+  });
+
+  describe('Get by ID', function() {
+
+  });
+
+  describe('Update by ID', function() {
+
+  });
+
+  describe('Delete by ID', function() {
+
+  });
+
+});
