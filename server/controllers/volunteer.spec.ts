42ea99de2ae8b406e3b0acda714043bd95896ad3
Ted Mader <ted.mader@ge.com>
2017-06-25 20:28:42 -0500

    Add test-server npm script and update volunteer spec

--- server/controllers/volunteer.spec.ts
+++ server/controllers/volunteer.spec.ts
@@ -1,69 +1,81 @@
 import * as request from 'request';
-import createSpy = jasmine.createSpy;
+import Volunteer from '../models/volunteer';
+import VolunteerController from './volunteer';
 
 const base_url = 'http://localhost:3000/api';
 
+Volunteer.prototype.find = function (callback) {
+  console.log('in the mock');
+  callback();
+};
+
 let req, res;
-beforeEach(function() {
+beforeEach(() => {
+  console.log('*** BEGIN beforeEach() ***');
   req = {
     query: {},
     params: {},
     body: {},
   };
-
   res = {
-    status: createSpy('status').and.callFake(function(msg) {
+    status: jasmine.createSpy('status').and.callFake((msg) => {
+      console.log(`res.status called with ${msg}`);
       return this;
     }),
-    send: createSpy('send').and.callFake(function(msg) {
+    send: jasmine.createSpy('send').and.callFake((msg) => {
+      console.log(`res.send called with ${msg}`);
       return this;
     })
   };
+  console.log(res.status(200));
+  console.log('*** END beforeEach() ***');
 });
 
-afterEach(function() {
-  expect(res.status.callCount).toEqual(1);
-  expect(res.send.callCount).toEqual(1);
+afterEach(() => {
+  expect(res.status.calls.count).toEqual(1);
+  expect(res.send.calls.count).toEqual(1);
 });
 
-describe('VolunteerController', function() {
+const volunteerController = new VolunteerController;
 
-  describe('Get all', function() {
+describe('VolunteerController', function () {
 
-    it('returns status code 200', function(done) {
-      request.get(`${base_url}/volunteers`, function(error, response) {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
+  describe('Get all', function () {
+
+    it('returns status code 200', function (done) {
+      volunteerController.getAll(req, res);
+      expect(Volunteer.find.calls.count).toEqual(1);
+      expect(res.status).toHaveBeenCalledWith(200);
+      done();
     });
 
   });
 
-  describe('Count all', function() {
-
-    it('returns status code 200', function(done) {
-      request.get(`${base_url}/volunteers`, function(error, response) {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-    });
-
-  });
-
-  describe('Insert', function() {
-
-  });
-
-  describe('Get by ID', function() {
-
-  });
-
-  describe('Update by ID', function() {
-
-  });
-
-  describe('Delete by ID', function() {
-
-  });
+  // describe('Count all', function () {
+  //
+  //   it('returns status code 200', function (done) {
+  //     request.get(`${base_url}/volunteers`, function (error, response) {
+  //       expect(response.statusCode).toBe(200);
+  //       done();
+  //     });
+  //   });
+  //
+  // });
+  //
+  // describe('Insert', function () {
+  //
+  // });
+  //
+  // describe('Get by ID', function () {
+  //
+  // });
+  //
+  // describe('Update by ID', function () {
+  //
+  // });
+  //
+  // describe('Delete by ID', function () {
+  //
+  // });
 
 });
