f4196b2df9f094db3194dcdd2775d5824911abb6
Ted Mader <tlmader.dev@gmail.com>
2017-06-29 16:29:41 -0500

    Import rxjs augmentations in service

--- client/app/shared/volunteer.service.ts
+++ client/app/shared/volunteer.service.ts
@@ -1,25 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http, RequestOptions } from '@angular/http';
+import { Headers, Http, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import Volunteer from './volunteer';
 import handleError from './handle-error';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class VolunteerService {
 
   constructor(private http: Http) { }
 
+  getVolunteers(): Observable<Volunteer[]> {
+    return this.http.get('/api/volunteers')
+      .map((res: Response) => res.json())
+      .catch(handleError);
+  }
+
   postVolunteer(volunteer): Observable<Volunteer> {
     const headers = new Headers({ 'Content-Type': 'application/json' });
     const options = new RequestOptions({ headers: headers });
     return this.http.post('/api/volunteer', volunteer, options)
-      .map(res => res.json())
-      .catch(handleError);
-  }
-
-  getVolunteers(): Observable<Volunteer[]> {
-    return this.http.get('/api/volunteers')
-      .map(res => res.json())
+      .map((res: Response) => res.json())
       .catch(handleError);
   }
 }
