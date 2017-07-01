9bf2405ba3758d9dae173b9e53073e698ed41f19
Ted Mader <tlmader.dev@gmail.com>
2017-06-30 21:37:51 -0500

    Create mock getVisits function

--- client/app/visit-history/visit-history.component.ts
+++ client/app/visit-history/visit-history.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Visit from '../shared/visit';
+import Volunteer from '../shared/volunteer';
 
 @Component({
   selector: 'app-visit-history',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visit-history.component.css']
 })
 export class VisitHistoryComponent implements OnInit {
+  public visits: Visit[];
 
   constructor() { }
 
   ngOnInit() {
+    this.visits = this.getVisits();
+  }
+
+  /**
+   * Use mock data until API is built
+   * @returns {[{volunteer: Volunteer, startedAt: Date, endedAt: Date}]}
+   */
+  getVisits(): Visit[] {
+    const testVolunteer: Volunteer = {
+      firstName: 'Ted',
+      lastName: 'Mader',
+      petName: 'Mimi'
+    };
+    return [
+      {
+        volunteer: testVolunteer,
+        startedAt: new Date('2017-06-29T12:45:02.336Z'),
+        endedAt: new Date('2017-06-29T18:45:02.336Z')
+      },
+      {
+        volunteer: testVolunteer,
+        startedAt: new Date('2017-06-29T12:45:02.336Z'),
+        endedAt: new Date('2017-06-29T18:45:02.336Z')
+      },
+      {
+        volunteer: testVolunteer,
+        startedAt: new Date('2017-06-29T12:45:02.336Z'),
+        endedAt: new Date('2017-06-29T18:45:02.336Z')
+      }
+    ]
   }
 
 }
