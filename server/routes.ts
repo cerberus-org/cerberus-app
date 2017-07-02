9161aa2527df2fd8eaecea457f9f3c1b72352084
Hillary Arurang <hillarurang@gmail.com>
2017-07-02 12:52:20 -0500

    Use proper handler and simplify timestamp declaration

--- server/routes.ts
+++ server/routes.ts
@@ -1,12 +1,14 @@
 import * as express from 'express';
 
 import VolunteerHandler from './handlers/volunteer';
+import VisitHandler from './handlers/visit';
 
 export default function setRoutes(app) {
 
   const router = express.Router();
 
   const volunteerHandler = new VolunteerHandler();
+  const visitHandler = new VisitHandler();
 
   // Volunteers
   router.route('/volunteers').get(volunteerHandler.getAll);
@@ -17,12 +19,12 @@ export default function setRoutes(app) {
   router.route('/volunteer/:id').delete(volunteerHandler.delete);
 
   // Visits
-  router.route('/visits').get(volunteerHandler.getAll);
-  router.route('/visit/count').get(volunteerHandler.count);
-  router.route('/visit').post(volunteerHandler.insert);
-  router.route('/visit/:id').get(volunteerHandler.get);
-  router.route('/visit/:id').put(volunteerHandler.update);
-  router.route('/visit/:id').delete(volunteerHandler.delete);
+  router.route('/visits').get(visitHandler.getAll);
+  router.route('/visit/count').get(visitHandler.count);
+  router.route('/visit').post(visitHandler.insert);
+  router.route('/visit/:id').get(visitHandler.get);
+  router.route('/visit/:id').put(visitHandler.update);
+  router.route('/visit/:id').delete(visitHandler.delete);
 
   // Apply the routes to our application with the prefix /api
   app.use('/api', router);
