2064947475eb216db9a891f247007b342e0a0fa9
Ted Mader <tlmader.dev@gmail.com>
2017-07-05 21:04:56 -0500

    Add margins to submit buttons

--- client/app/volunteer-check-in/volunteer-check-in.component.spec.ts
+++ client/app/volunteer-check-in/volunteer-check-in.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VolunteerCheckInComponent } from './volunteer-check-in.component';
 import { MockComponent } from 'ng2-mock-component';
+import { MdTabsModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('VolunteerCheckInComponent', () => {
   let component: VolunteerCheckInComponent;
@@ -13,6 +15,10 @@ describe('VolunteerCheckInComponent', () => {
         VolunteerCheckInComponent,
         MockComponent({ selector: 'app-check-in-form' }),
         MockComponent({ selector: 'app-new-volunteer-form' })
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        MdTabsModule
       ]
     }).compileComponents();
   }));
