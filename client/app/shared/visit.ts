2f264156a6b2acab6534a3563454835697b4a593
Ted Mader <tlmader.dev@gmail.com>
2017-07-06 19:46:21 -0500

    Use volunteerId in visit class

--- client/app/shared/visit.ts
+++ client/app/shared/visit.ts
@@ -1,7 +1,5 @@
-import { testVolunteers, Volunteer } from './volunteer';
-
 export class Visit {
-  volunteer: Volunteer;
+  volunteerId: string;
   startedAt: Date;
   endedAt: Date;
   timezone: string;
@@ -9,19 +7,19 @@ export class Visit {
 
 export const testVisits: Visit[] = [
   {
-    volunteer: testVolunteers[0],
+    volunteerId: '5955737c88a8dde94e2e2ed4',
     startedAt: new Date('2017-06-29T10:45:02.336Z'),
     endedAt: new Date('2017-06-29T16:45:56.336Z'),
     timezone: 'America/Chicago'
   },
   {
-    volunteer: testVolunteers[1],
+    volunteerId: '5955737c88a8dde94e2e2ed4',
     startedAt: new Date('2017-06-29T12:45:42.336Z'),
     endedAt: new Date('2017-06-29T18:45:01.336Z'),
     timezone: 'America/New_York'
   },
   {
-    volunteer: testVolunteers[0],
+    volunteerId: '5955737c88a8dde94e2e2ed4',
     startedAt: new Date('2017-06-30T12:45:42.336Z'),
     endedAt: new Date('2017-06-30T18:45:01.336Z'),
     timezone: 'America/Chicago'
