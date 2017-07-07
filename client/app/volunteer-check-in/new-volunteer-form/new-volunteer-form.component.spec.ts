1ffe62cce530a4ddc28b9273f2c49d2e94a5e0a3
Ted Mader <tlmader.dev@gmail.com>
2017-07-06 23:57:52 -0500

    Create specs for new volunteer form validators

--- client/app/volunteer-check-in/new-volunteer-form/new-volunteer-form.component.spec.ts
+++ client/app/volunteer-check-in/new-volunteer-form/new-volunteer-form.component.spec.ts
@@ -1,24 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NewVolunteerFormComponent } from './new-volunteer-form.component';
-// modules
 import { RouterTestingModule } from '@angular/router/testing';
-// angular material
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdAutocompleteModule, MdInputModule } from '@angular/material';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import 'hammerjs';
-// volunteer service
 import { HttpModule } from '@angular/http';
 
 describe('NewVolunteerFormComponent', () => {
-  let component: NewVolunteerFormComponent;
-  let fixture: ComponentFixture<NewVolunteerFormComponent>;
-
-  let emptyForm = {
-    firstName: null,
-    lastName: null,
-    petName: null
-  }
+  let component: NewVolunteerFormComponent,
+    fixture: ComponentFixture<NewVolunteerFormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -47,8 +38,51 @@ describe('NewVolunteerFormComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('form should clear on submit', (() => {
-    component.addVolunteer();
-    expect(component.newVolunteerForm.value).toEqual(emptyForm);
-  }));
+  ['firstName', 'lastName', 'petName'].forEach(form => {
+    describe((`${form} control`), () => {
+      let control: AbstractControl;
+
+      beforeEach(async(() => {
+        control = component.formGroup.controls[form];
+      }));
+
+      it('should validate requirement', (() => {
+        expect(control.valid).toBeFalsy();
+        const errors = control.errors || {};
+        expect(errors['required']).toBeTruthy();
+      }));
+
+      it('should validate min length', (() => {
+        control.setValue('C');
+        const errors = control.errors || {};
+        expect(errors['required']).toBeFalsy();
+        expect(errors['minlength']).toBeTruthy();
+      }));
+
+      it('should validate max length', (() => {
+        control.setValue('Quinquagintaquadringentilliards');
+        const errors = control.errors || {};
+        expect(errors['required']).toBeFalsy();
+        expect(errors['maxlength']).toBeTruthy();
+      }));
+
+      it('should validate pattern', (() => {
+        control.setValue('!@#$%^&*()_+');
+        const errors = control.errors || {};
+        expect(errors['required']).toBeFalsy();
+        expect(errors['pattern']).toBeTruthy();
+      }));
+
+      it('should accept a valid name', (() => {
+        control.setValue('Cerberus');
+        expect(control.errors).toBeFalsy();
+      }));
+
+      it('should clear the form on submit', (() => {
+        control.setValue('Cerberus');
+        component.onSubmit();
+        expect(control.value).toBeFalsy();
+      }));
+    });
+  })
 });
