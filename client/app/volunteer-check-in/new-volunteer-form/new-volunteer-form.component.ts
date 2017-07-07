8460cdebdc2d5ccb7fd0ad35c614e9cd78e32eb6
Ted Mader <tlmader.dev@gmail.com>
2017-07-06 22:57:42 -0500

    Add regex and length validators to new volunteer form

--- client/app/volunteer-check-in/new-volunteer-form/new-volunteer-form.component.ts
+++ client/app/volunteer-check-in/new-volunteer-form/new-volunteer-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { VolunteerService } from '../../shared/volunteer.service';
@@ -35,10 +35,12 @@ export class NewVolunteerFormComponent implements OnInit {
       { placeholder: 'Last', control: 'lastName' },
       { placeholder: 'Favorite Pet Name', control: 'petName' }
     ];
+    const regex = /^[a-z ,.'-]+$/i;
+    const validators = [Validators.required, Validators.pattern(regex), Validators.minLength(2), Validators.maxLength(30)];
     this.newVolunteerForm = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      petName: ['', Validators.required]
+      firstName: ['', validators],
+      lastName: ['', validators],
+      petName: ['', validators]
     });
   }
 }
