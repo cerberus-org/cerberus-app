fb5a1ca6867ef66c7d67270619bca078acfa1576
Hillary Arurang <hillarurang@gmail.com>
2017-07-08 13:05:43 -0500

    Protect single api

--- server/routes.ts
+++ server/routes.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 
 import VolunteerHandler from './handlers/volunteer';
 import VisitHandler from './handlers/visit';
+import Volunteer from './models/volunteer';
 
 export default function setRoutes(app) {
 
@@ -11,7 +12,7 @@ export default function setRoutes(app) {
   const visitHandler = new VisitHandler();
 
   // Volunteers
-  router.route('/volunteers').get(volunteerHandler.getAll);
+  // router.route('/volunteers').get(volunteerHandler.getAll);
   router.route('/volunteers/count').get(volunteerHandler.count);
   router.route('/volunteer').post(volunteerHandler.insert);
   router.route('/volunteer/:id').get(volunteerHandler.get);
@@ -26,6 +27,24 @@ export default function setRoutes(app) {
   router.route('/visit/:id').put(visitHandler.update);
   router.route('/visit/:id').delete(visitHandler.delete);
   router.route('/visits/:date').get(visitHandler.getByDate);
+
+  // Test
+  app.get('/volunteers', (req, res, next) => {
+    // validation
+    if (req.user) {
+      next();
+    } else {
+      res.status(401).json(JSON.stringify({'Message': 'Unauthorized.'}));
+    }
+    // api call
+    Volunteer.find({}, (err, docs) => {
+      if (err) {
+        res.status(400).send(err);
+        return console.error(err);
+      }
+      res.json(docs);
+    });
+  });
 
   // Apply the routes to our application with the prefix /api
   app.use('/api', router);
