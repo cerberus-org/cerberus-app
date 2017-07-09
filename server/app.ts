cfdaf399c075060a90715df1024afa2e25a02a41
Hillary Arurang <hillarurang@gmail.com>
2017-07-08 19:19:10 -0500

    Use correct library for jwt

--- server/app.ts
+++ server/app.ts
@@ -2,11 +2,13 @@ import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as mongoose from 'mongoose';
 import * as path from 'path';
+import 'zone.js';
+import 'reflect-metadata';
 
 import setRoutes from './routes';
 
 const app = express();
-const jwt = require('angular2-jwt');
+const jwt = require('express-jwt');
 
 app.set('port', (process.env.PORT || 3000));
 
@@ -35,7 +37,7 @@ db.once('open', () => {
 });
 
 // Add a catch all route to set the req.user property
-// unsure if app.config
-app.use(jwt({secret: app.config.get('sessionSecret'), credentialsRequired: false}));
+// unsure about secret
+app.use(jwt({secret: 'test', credentialsRequired: false}));
 
 export { app };
