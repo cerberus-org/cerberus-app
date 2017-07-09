ee9a66b265f0b8c40223a9f2fb5322f54d918b25
Hillary Arurang <hillarurang@gmail.com>
2017-07-08 22:35:35 -0500

    Add questions in comments

--- server/handlers/user.ts
+++ server/handlers/user.ts
@@ -9,12 +9,13 @@ export default class UserHandler extends BaseHandler {
   model = User;
 
   login = (req, res) => {
-    console.log('Made it');
     this.model.findOne({ email: req.body.email }, (err, user) => {
       if (!user) { return res.sendStatus(403); }
       user.comparePassword(req.body.password, (error, isMatch) => {
         if (!isMatch) { return res.sendStatus(403); }
-        const token = jwt.sign({ user: user }, 'test'); // , { expiresIn: 10 } seconds
+        // why sign with user
+        // why do I need test
+        const token = jwt.sign({ user: user }, 'test');
         res.status(200).json({ token: token });
       });
     });
