dbc80de63a12dac6786ee7bb015cbe5fa33bb102
Hillary Arurang <hillarurang@gmail.com>
2017-07-10 20:40:24 -0500

    Add mock router for login component test

--- client/app/login/login.component.spec.ts
+++ client/app/login/login.component.spec.ts
@@ -2,25 +2,34 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MdInputModule } from '@angular/material';
+import { MdInputModule, MdListModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import 'hammerjs';
 import { LoginService, MockLoginService } from '../shared/login.service';
+import { Router } from '@angular/router';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  class MockRouter {
+    navigate = jasmine.createSpy('navigate');
+  }
+
+  let mockRouter: MockRouter;
+
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
+      mockRouter = new MockRouter(),
+      TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
       imports: [
         FormsModule,
         ReactiveFormsModule,
         MdInputModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        MdListModule
       ],
-      providers: [{ provide: LoginService, useClass: MockLoginService }]
+      providers: [{ provide: LoginService, useClass: MockLoginService }, { provide: Router, useValue: mockRouter }]
     })
     .compileComponents();
   }));
