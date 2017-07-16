ccc1b9d8497f676e5518307b3cc75ae49b7312fc
Ted Mader <tlmader.dev@gmail.com>
2017-07-16 14:28:40 -0500

    Update tsconfig.json to target es6

--- client/app/visit-history/visit-history.component.ts
+++ client/app/visit-history/visit-history.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Volunteer } from '../models/volunteer';
-import { testVisits, Visit } from '../models/visit';
+import { Visit } from '../models/visit';
 import * as moment from 'moment-timezone';
 import { VisitService } from '../services/visit.service';
 
