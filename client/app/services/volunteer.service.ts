152de806e32ebbeacc6e602ee01087716c2c2191
Hillary Arurang <hillarurang@gmail.com>
2017-07-19 08:43:57 -0500

    Create rx for add volunteer in service

--- client/app/services/volunteer.service.ts
+++ client/app/services/volunteer.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
+import { Store } from '@ngrx/store';
+import { ADD_VOLUNTEER, LOAD_VOLUNTEERS, MODIFY_VOLUNTEER } from '../reducers/volunteer';
 
 import BaseService from './base.service';
 import { testVolunteers, Volunteer } from '../models/volunteer';
@@ -11,15 +13,22 @@ export class VolunteerService extends BaseService {
   model = Volunteer;
   modelName = 'volunteer';
 
-  constructor(http: Http) {
+  constructor(http: Http, private store: Store<Volunteer[]>) {
     super(http);
+  }
+
+  createRx(obj: any): void {
+    this.http.post(`/api/${this.modelName}`, JSON.stringify(obj), this.options)
+      .map(res => this.convert(res.json()))
+      .map(payload => ({ type: ADD_VOLUNTEER, payload: payload }))
+      .subscribe(action => this.store.dispatch(action));
   }
 }
 
 export class MockVolunteerService extends VolunteerService {
 
   constructor() {
-    super(null);
+    super(null, null);
   }
 
   getAll(): Observable<Volunteer[]> {
