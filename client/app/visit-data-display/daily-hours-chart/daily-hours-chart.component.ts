e3376d4ca50c3edcea8888a187ea6811e719b02e
Ted Mader <ted.mader@ge.com>
2017-08-06 20:06:42 -0500

    Implement function for setting line chart data

--- client/app/visit-data-display/daily-hours-chart/daily-hours-chart.component.ts
+++ client/app/visit-data-display/daily-hours-chart/daily-hours-chart.component.ts
@@ -8,7 +8,6 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./daily-hours-chart.component.css']
 })
 export class DailyHoursChartComponent implements OnInit {
-  visits: Visit[];
   visitsByDate: Map<string, Visit[]>;
   lineChartData: number[];
   lineChartLabels: string[];
@@ -18,13 +17,18 @@ export class DailyHoursChartComponent implements OnInit {
 
   ngOnInit() {
     this.subscribeToVisits();
-    this.mapVisitsToDate(this.visits);
   }
 
   subscribeToVisits(): void {
     this.store.select<Visit[]>('visits').subscribe(
-      visits => this.visits = visits,
-      error => this.error = <any>error);
+      visits => {
+        this.mapVisitsToDate(visits);
+        this.setLineChartLabels();
+        this.setLineChartData();
+        console.log(this.visitsByDate);
+        console.log(this.lineChartLabels);
+        console.log(this.lineChartData);
+      }, error => this.error = <any>error);
   }
 
   mapVisitsToDate(visits) {
@@ -40,11 +44,22 @@ export class DailyHoursChartComponent implements OnInit {
   }
 
   setLineChartLabels(): void {
-    const date = new Date();
-    this.lineChartLabels = Array.from(Array(10), (_, i) => {
+    this.lineChartLabels = Array.from(Array(7), (_, i) => {
+      const date = new Date();
       date.setDate(date.getDate() - i);
       return date.toDateString();
     });
-    console.log(this.lineChartLabels);
   }
+
+  setLineChartData(): void {
+    this.lineChartData = this.lineChartLabels.map(date => this.visitsByDate.has(date)
+      ? Math.floor(this.visitsByDate.get(date)
+        .reduce((total, currentVisit) => total + this.getDuration(currentVisit), 0) / 3600000)
+      : null);
+  }
+
+  getDuration(visit: Visit): number {
+    return visit.endedAt.getTime() - visit.startedAt.getTime();
+  }
+
 }
