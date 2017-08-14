03c33c579a3579294f31d9aa193e410315f6ee5c
Hillary Arurang <hillarurang@gmail.com>
2017-08-13 19:34:15 -0500

    Make email and password required fields

--- client/app/login/login.component.ts
+++ client/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router';
 import { UserService } from '../services/user.service';
 import { VisitService } from '../services/visit.service';
@@ -37,8 +37,8 @@ export class LoginComponent implements OnInit {
   createForm() {
     this.loginForm = this.fb.group({
       // list form controls
-      email: '',
-      password: '',
+      email: ['', Validators.required],
+      password: ['', Validators.required]
     });
   }
 }
