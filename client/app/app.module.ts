4f332435d5ece6e5f169230f452a747921fa9a7b
Ted Mader <tlmader.dev@gmail.com>
2017-08-14 23:16:55 -0500

    Organize app.module

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,55 +1,44 @@
-import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
-  MdAutocompleteModule,
-  MdButtonModule,
-  MdCardModule,
-  MdInputModule,
-  MdListModule, MdPaginatorModule,
-  MdTableModule,
-  MdTabsModule,
-  MdToolbarModule
+  MdAutocompleteModule, MdButtonModule, MdCardModule, MdInputModule, MdListModule, MdPaginatorModule, MdTableModule,
+  MdTabsModule, MdToolbarModule
 } from '@angular/material';
 import { CdkTableModule } from '@angular/cdk';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
-import { SignaturePadModule } from 'angular2-signaturepad';
 import { ChartsModule } from 'ng2-charts';
 import { Guard } from './guard';
+import { SignaturePadModule } from 'angular2-signaturepad';
 import 'hammerjs';
+
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
 import { LayoutComponent } from './shared/layout/layout.component';
-import { LoginComponent } from './login/login.component';
-import { VisitHistoryTableComponent } from './visit-data-display/visit-history-table/visit-history-table.component';
-import { NewVolunteerFormComponent } from './volunteer-check-in/new-volunteer-form/new-volunteer-form.component';
-import { CheckInFormComponent } from './volunteer-check-in/check-in-form/check-in-form.component';
-import { VolunteerCheckInComponent } from './volunteer-check-in/volunteer-check-in.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
-import { VolunteerMenuComponent } from './home/volunteer-menu/volunteer-menu.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
-import { VisitDataDisplayComponent } from './visit-data-display/visit-data-display.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { VolunteerMenuComponent } from './home/volunteer-menu/volunteer-menu.component';
+import { VolunteerCheckInComponent } from './volunteer-check-in/volunteer-check-in.component';
+import { CheckInFormComponent } from './volunteer-check-in/check-in-form/check-in-form.component';
 import { SignatureFieldComponent } from './signature-field/signature-field.component';
+import { NewVolunteerFormComponent } from './volunteer-check-in/new-volunteer-form/new-volunteer-form.component';
+import { VisitDataDisplayComponent } from './visit-data-display/visit-data-display.component';
+import { VisitHistoryTableComponent } from './visit-data-display/visit-history-table/visit-history-table.component';
+import { DailyHoursChartComponent } from './visit-data-display/daily-hours-chart/daily-hours-chart.component';
 
 import { UserService } from './services/user.service';
 import { VolunteerService } from './services/volunteer.service';
 import { VisitService } from './services/visit.service';
-import { StoreModule } from '@ngrx/store';
-import { HeaderComponent } from './shared/layout/header/header.component';
-import { VolunteerMenuComponent } from './home/volunteer-menu/volunteer-menu.component';
-import { FooterComponent } from './shared/layout/footer/footer.component';
-import { VisitDataDisplayComponent } from './visit-data-display/visit-data-display.component';
-import { DailyHoursChartComponent } from './visit-data-display/daily-hours-chart/daily-hours-chart.component';
 
 import { visitReducer } from './reducers/visit';
 import { volunteerReducer } from './reducers/volunteer';
-import { CdkTableModule } from '@angular/cdk';
 
 @NgModule({
   declarations: [
@@ -65,8 +54,8 @@ import { CdkTableModule } from '@angular/cdk';
     VolunteerMenuComponent,
     FooterComponent,
     VisitDataDisplayComponent,
-    DailyHoursChartComponent
-    SignatureFieldComponent,
+    DailyHoursChartComponent,
+    SignatureFieldComponent
   ],
   imports: [
     BrowserModule,
@@ -86,8 +75,8 @@ import { CdkTableModule } from '@angular/cdk';
     MdTabsModule,
     MdToolbarModule,
     CdkTableModule,
-    SignaturePadModule,
     ChartsModule,
+    SignaturePadModule,
     StoreModule.provideStore({ visits: visitReducer, volunteers: volunteerReducer })
   ],
   providers: [
