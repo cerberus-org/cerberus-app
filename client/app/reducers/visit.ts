ebadbba59d215e206b7e1ca055ab70b0a25ab340
Hillary Arurang <hillarurang@gmail.com>
2017-08-31 09:25:35 -0500

    Filter out invalid visits

--- client/app/reducers/visit.ts
+++ client/app/reducers/visit.ts
@@ -4,12 +4,13 @@ import { Visit } from '../models/visit';
 export const LOAD_VISITS = 'LOAD_VISITS';
 export const ADD_VISIT = 'ADD_VISIT';
 export const MODIFY_VISIT = 'MODIFY_VISIT';
+const maxHours = 8;
 
 export function visitReducer(state: Visit[] = [], action: Action) {
   switch (action.type) {
     case 'LOAD_VISITS':
       // Reverse to order from newest to oldest
-      return action.payload.reverse();
+      return getValidVisits(action.payload).reverse();
     case 'ADD_VISIT':
       return [action.payload, ...state];
     case 'MODIFY_VISIT':
@@ -19,5 +20,28 @@ export function visitReducer(state: Visit[] = [], action: Action) {
     default:
       return state;
   }
+}
+
+function getValidVisits (visits: Visit[]): Visit[] {
+  // visits to discard
+  const invalidVisits: Visit[] = new Array();
+  // visits to return
+  const validVisits = visits.filter(function(visit){
+    // if the visit is still ongoing
+    if (visit.endedAt === null) {
+      // get current visit hours
+      const hours = Math.abs(visit.startedAt.getTime() - new Date().getTime()) / 3600000;
+      if (hours > maxHours) {
+        invalidVisits.push(visit);
+      } else {
+        // if the visit is valid keep it
+        return true;
+      }
+    } else {
+      // if the visit is complete keep it
+      return true;
+    }
+  });
+  return validVisits;
 }
 
