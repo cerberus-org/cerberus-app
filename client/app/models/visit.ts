b0d21aa4af5e53945d61be595c2fc05c6862777a
Ted Mader <ted.mader@ge.com>
2017-09-15 14:40:18 -0500

    Create location client model and update existing models

--- client/app/models/visit.ts
+++ client/app/models/visit.ts
@@ -1,5 +1,7 @@
 export class Visit {
   _id: string;
+  organizationId: string;
+  locationId: string;
   volunteerId: string;
   startedAt: Date;
   endedAt: Date;
@@ -18,6 +20,8 @@ export class Visit {
 export const testVisits: Visit[] = [
   {
     _id: '674a861ace7ca574af9070c0',
+    organizationId: '59a7055733bfe28af47cff40',
+    locationId: '59bc1e7ad92a6ac6f6252bfa',
     volunteerId: '5961327dfba1ca1b64b8945a',
     startedAt: new Date('2017-06-29T10:45:02.336Z'),
     endedAt: new Date('2017-06-29T14:45:02.336Z'),
@@ -26,6 +30,8 @@ export const testVisits: Visit[] = [
   },
   {
     _id: '674a861ace7ca574af9070c1',
+    organizationId: '59a7055733bfe28af47cff40',
+    locationId: '59bc1e7ad92a6ac6f6252bfa',
     volunteerId: '5961327dfba1ca1b64b8945b',
     startedAt: new Date('2017-06-29T12:45:42.336Z'),
     endedAt: new Date('2017-06-29T18:45:01.336Z'),
@@ -34,6 +40,8 @@ export const testVisits: Visit[] = [
   },
   {
     _id: '674a861ace7ca574af9070c2',
+    organizationId: '59a7055733bfe28af47cff40',
+    locationId: '59bc1e7ad92a6ac6f6252bfa',
     volunteerId: '5961327dfba1ca1b64b8945c',
     startedAt: new Date('2017-06-30T12:45:42.336Z'),
     endedAt: new Date('2017-06-30T18:45:01.336Z'),
@@ -42,6 +50,8 @@ export const testVisits: Visit[] = [
   },
   {
     _id: '674a861ace7ca574af9070c3',
+    organizationId: '59a7055733bfe28af47cff40',
+    locationId: '59bc1e7ad92a6ac6f6252bfa',
     volunteerId: '5961327dfba1ca1b64b8945a',
     startedAt: new Date('2017-07-01T14:45:42.336Z'),
     endedAt: null,
@@ -50,6 +60,8 @@ export const testVisits: Visit[] = [
   },
   {
     _id: '674a861ace7ca574af9070c4',
+    organizationId: '59a7055733bfe28af47cff40',
+    locationId: '59bc1e7ad92a6ac6f6252bfa',
     volunteerId: '5961327dfba1ca1b64b8945z',
     startedAt: new Date('2017-07-02T10:45:42.336Z'),
     endedAt: new Date('2017-07-02T23:45:01.336Z'),
