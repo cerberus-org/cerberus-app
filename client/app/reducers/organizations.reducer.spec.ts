ede29d69f2ccd7ee8cca231c5860d7902bba778c
Ted Mader <tlmader.dev@gmail.com>
2017-10-05 19:25:13 -0500

    Add describes to action specs

--- client/app/reducers/organizations.reducer.spec.ts
+++ client/app/reducers/organizations.reducer.spec.ts
@@ -7,24 +7,32 @@ describe('organizationReducer', () => {
 
   beforeEach(() => {
     organizations = testOrganizations.slice(0);
-
   });
 
-  it('loads organizations', () => {
-    const result = fromOrganizations.reducer({ organizations: organizations }, new OrganizationActions.Load(organizations)).organizations;
-    expect(result).toBe(organizations);
+  describe('LOAD', () => {
+
+    it('loads organizations', () => {
+      const result = fromOrganizations.reducer({ organizations: organizations }, new OrganizationActions.Load(organizations)).organizations;
+      expect(result).toBe(organizations);
+    });
   });
 
-  it('adds a organization', () => {
-    const organization = Object.assign({}, organizations[0]);
-    const result = fromOrganizations.reducer({ organizations: organizations }, new OrganizationActions.Add(organization)).organizations;
-    expect(result[0]).toBe(organization);
-    expect(result.length).toBe(organizations.length + 1);
+  describe('ADD', () => {
+
+    it('adds a organization', () => {
+      const organization = Object.assign({}, organizations[0]);
+      const result = fromOrganizations.reducer({ organizations: organizations }, new OrganizationActions.Add(organization)).organizations;
+      expect(result[0]).toBe(organization);
+      expect(result.length).toBe(organizations.length + 1);
+    });
   });
 
-  it('modifies a organization', () => {
-    const modified = Object.assign({}, organizations[0]);
-    const result = fromOrganizations.reducer({ organizations: organizations }, new OrganizationActions.Modify(modified)).organizations;
-    expect(result[0]).toBe(modified);
+  describe('MODIFY', () => {
+
+    it('modifies a organization', () => {
+      const modified = Object.assign({}, organizations[0]);
+      const result = fromOrganizations.reducer({ organizations: organizations }, new OrganizationActions.Modify(modified)).organizations;
+      expect(result[0]).toBe(modified);
+    });
   });
 });
