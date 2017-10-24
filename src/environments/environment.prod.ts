646996ac0f25997ce92f3997f1ceccd23d3fdb58
Ted Mader <tlmader.dev@gmail.com>
2017-10-24 01:19:03 -0500

    Update Travis and production env settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCDge4EiLnuYR4ARpPvSpVylvZBIz4wsjE",
+    authDomain: "cerberus-cloud.firebaseapp.com",
+    databaseURL: "https://cerberus-cloud.firebaseio.com",
+    projectId: "cerberus-cloud",
+    storageBucket: "cerberus-cloud.appspot.com",
+    messagingSenderId: "506182479033"
+  }
 };
