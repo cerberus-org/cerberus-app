cfbd1a905113855e2ea219731eb0db96ac525b0b
Ted Mader <ted.mader@ge.com>
2017-10-23 23:27:40 -0500

    Clear form data state after check in/out success

--- client/app/actions/check-in.actions.ts
+++ client/app/actions/check-in.actions.ts
@@ -5,10 +5,14 @@ import { Volunteer } from '../models/volunteer';
 
 export const LOAD_DATA = '[Check-In] Load data';
 export const LOAD_DATA_SUCCESS = '[Check-In] Load data success';
+
 export const SUBMIT_NEW_VOLUNTEER = '[Check-In] Submit new volunteer';
 export const SUBMIT_NEW_VOLUNTEER_SUCCESS = '[Check-In] Submit new volunteer success';
+
 export const CHECK_IN = '[Check-In] Check in';
 export const CHECK_OUT = '[Check-In] Check out';
+export const CHECK_IN_OR_OUT_SUCCESS = '[Check-In] Check in or out success';
+
 export const FILTER_AND_SELECT_VOLUNTEERS_BY_NAME = '[Check-In] Filter and select volunteers by name 👱';
 export const SELECT_VOLUNTEER_BY_PET_NAME = '[Check-In] Select volunteer by petName 🐶';
 export const SELECT_ACTIVE_VISIT_FOR_VOLUNTEER = '[Check-In] Select active visit for volunteer';
@@ -55,6 +59,12 @@ export class CheckOut implements Action {
   constructor(public payload: Visit) {}
 }
 
+export class CheckInOrOutSuccess implements Action {
+  readonly type = CHECK_IN_OR_OUT_SUCCESS;
+
+  constructor(public payload: any) {}
+}
+
 export class FilterAndSelectVolunteersByName implements Action {
   readonly type = FILTER_AND_SELECT_VOLUNTEERS_BY_NAME;
 
@@ -80,6 +90,7 @@ export type All
   | SubmitNewVolunteerSuccess
   | CheckIn
   | CheckOut
+  | CheckInOrOutSuccess
   | FilterAndSelectVolunteersByName
   | SelectVolunteerByPetName
   | SelectActiveVisitForVolunteer;
