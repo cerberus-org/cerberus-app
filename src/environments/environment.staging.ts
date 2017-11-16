728fb03883be14aaae87d4a6c4eba7fee684bde4
Ted Mader <tlmader.dev@gmail.com>
2017-11-16 14:02:46 -0600

    Add staging environment configuration

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCWBfsX_m0r_0LJ8QOAJv6xKkMODSfoTc4",
+    authDomain: "cerberus-staging.firebaseapp.com",
+    databaseURL: "https://cerberus-staging.firebaseio.com",
+    projectId: "cerberus-staging",
+    storageBucket: "cerberus-staging.appspot.com",
+    messagingSenderId: "765294677808"
+  }
+};
