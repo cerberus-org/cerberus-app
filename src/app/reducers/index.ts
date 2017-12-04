dd6c14f9d29507dc27d6e489b00c8fd02467ee8c
Ted Mader <ted.mader@ge.com>
2017-12-04 10:36:51 -0600

    Add @ngrx/router-store dependency

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -2,11 +2,13 @@ import { ActionReducerMap } from '@ngrx/store';
 import * as fromCheckIn from './check-in.reducer';
 import * as fromDataDisplay from './data-display.reducer';
 import * as fromGettingStarted from './getting-started.reducer';
+import * as fromRouter from '@ngrx/router-store';
 
 export interface AppState {
   checkIn: fromCheckIn.State;
   dataDisplay: fromDataDisplay.State;
   gettingStarted: fromGettingStarted.State;
+  routerReducer: fromRouter.RouterReducerState;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
