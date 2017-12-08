aada02073310952ea9279660c34e8cf54bdfb69e
Ted Mader <ted.mader@ge.com>
2017-12-08 14:18:04 -0600

    Refactor check-in reducer specs

--- src/app/components/check-in-form/check-in-form.component.spec.ts
+++ src/app/components/check-in-form/check-in-form.component.spec.ts
@@ -52,6 +52,55 @@ describe('CheckInFormComponent', () => {
     expect(component.formGroup.controls['petName']).toBeTruthy();
   });
 
+  it('filters by name', () => {
+    const name = testVolunteers[1].firstName;
+    const state = fromCheckIn.reducer(
+      testState,
+      new CheckInActions.FilterAndSelectVolunteersByName(name)
+    );
+    expect(state.filteredVolunteers[0]).toBe(testVolunteers[1]);
+  });
+
+  it('creates the list of unique names for the filtered testVolunteers', () => {
+    const name = testVolunteers[0].firstName;
+    const state = fromCheckIn.reducer(testState, new CheckInActions.FilterAndSelectVolunteersByName(name));
+    expect(state.filteredUniqueNames.length).toEqual(1);
+    expect(state.filteredUniqueNames[0]).toBe(`${testVolunteers[0].firstName} ${testVolunteers[0].lastName}`);
+  });
+
+  it('checks if the filtered testVolunteers all match the same name', () => {
+    const name = `${testVolunteers[0].firstName} ${testVolunteers[0].lastName}`;
+    const state = fromCheckIn.reducer(testState, new CheckInActions.FilterAndSelectVolunteersByName(name));
+    expect(state.filteredAllMatchSameName).toBeTruthy();
+  });
+
+  it('does not select if the name does not exactly match', () => {
+    const name = testVolunteers[1].firstName;
+    const state = fromCheckIn.reducer(testState, new CheckInActions.FilterAndSelectVolunteersByName(name));
+    expect(state.selectedVolunteer).toBeFalsy();
+  });
+
+  it('selects a volunteer by name', () => {
+    const name = `${testVolunteers[1].firstName} ${testVolunteers[1].lastName}`;
+    const state = fromCheckIn.reducer(testState, new CheckInActions.FilterAndSelectVolunteersByName(name));
+    expect(state.selectedVolunteer).toBe(testVolunteers[1]);
+  });
+
+  it('selects a volunteer by petName', () => {
+    const petName = testVolunteers[2].petName;
+    const state = fromCheckIn.reducer(testState, new CheckInActions.SelectVolunteerByPetName(petName));
+    expect(state.selectedVolunteer).toBe(testVolunteers[2]);
+  });
+
+  it('selects an active visit for a volutneer', () => {
+    const volunteer = Object.assign({}, testVolunteers[0]);
+    const state = fromCheckIn.reducer(
+      testState,
+      new CheckInActions.SelectActiveVisitForVolunteer(volunteer)
+    );
+    expect(state.selectedVisit).toBe(testVisits[3]);
+  });
+
   describe('name control', () => {
 
     beforeEach(() => {
--- src/app/reducers/check-in.reducer.spec.ts
+++ src/app/reducers/check-in.reducer.spec.ts
@@ -1,13 +1,14 @@
-import { testVisits } from '../models/visit';
-import { testVolunteers } from '../models/volunteer';
 import * as fromCheckIn from './check-in.reducer'
 import * as CheckInActions from '../actions/check-in.actions';
+import { testVisits } from '../models/visit';
+import { testVolunteers } from '../models/volunteer';
 
 describe('checkInReducer', () => {
   let testState;
 
   beforeEach(() => {
     testState = Object.assign({}, fromCheckIn.initialState, {
+      selectedTabIndex: 1,
       visits: testVisits,
       volunteers: testVolunteers
     });
@@ -27,61 +28,16 @@ describe('checkInReducer', () => {
     });
   });
 
-  describe('SELECT_ACTIVE_VISIT_FOR_VOLUNTEER', () => {
+  describe('SUBMIT_NEW_VOLUNTEER_SUCCESS', () => {
 
-    it('selects an active visit for a volutneer', () => {
-      const volunteer = Object.assign({}, testVolunteers[0]);
+    it('sets the tab index', () => {
       const state = fromCheckIn.reducer(
-        testState,
-        new CheckInActions.SelectActiveVisitForVolunteer(volunteer)
-      );
-      expect(state.selectedVisit).toBe(testVisits[3]);
-    });
-  });
-
-  describe('FILTER_AND_SELECT_VOLUNTEERS_BY_NAME', () => {
-
-    it('filters by name', () => {
-      const name = testVolunteers[1].firstName;
-      const state = fromCheckIn.reducer(
-        testState,
-        new CheckInActions.FilterAndSelectVolunteersByName(name)
-      );
-      expect(state.filteredVolunteers[0]).toBe(testVolunteers[1]);
-    });
-
-    it('creates the list of unique names for the filtered testVolunteers', () => {
-      const name = testVolunteers[0].firstName;
-      const state = fromCheckIn.reducer(testState, new CheckInActions.FilterAndSelectVolunteersByName(name));
-      expect(state.filteredUniqueNames.length).toEqual(1);
-      expect(state.filteredUniqueNames[0]).toBe(`${testVolunteers[0].firstName} ${testVolunteers[0].lastName}`);
-    });
-
-    it('checks if the filtered testVolunteers all match the same name', () => {
-      const name = `${testVolunteers[0].firstName} ${testVolunteers[0].lastName}`;
-      const state = fromCheckIn.reducer(testState, new CheckInActions.FilterAndSelectVolunteersByName(name));
-      expect(state.filteredAllMatchSameName).toBeTruthy();
-    });
-
-    it('does not select if the name does not exactly match', () => {
-      const name = testVolunteers[1].firstName;
-      const state = fromCheckIn.reducer(testState, new CheckInActions.FilterAndSelectVolunteersByName(name));
-      expect(state.selectedVolunteer).toBeFalsy();
-    });
-
-    it('selects if the name exactly matches', () => {
-      const name = `${testVolunteers[1].firstName} ${testVolunteers[1].lastName}`;
-      const state = fromCheckIn.reducer(testState, new CheckInActions.FilterAndSelectVolunteersByName(name));
-      expect(state.selectedVolunteer).toBe(testVolunteers[1]);
-    });
-  });
-
-  describe('SELECT_VOLUNTEER_BY_PET_NAME', () => {
-
-    it('selects if the name exactly matches', () => {
-      const petName = testVolunteers[2].petName;
-      const state = fromCheckIn.reducer(testState, new CheckInActions.SelectVolunteerByPetName(petName));
-      expect(state.selectedVolunteer).toBe(testVolunteers[2]);
+        fromCheckIn.initialState,
+        new CheckInActions.SubmitNewVolunteerSuccess({
+          visits: testVisits,
+          volunteers: testVolunteers
+        }));
+      expect(state.selectedTabIndex).toEqual(0);
     });
   });
 });
