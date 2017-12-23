7e88ed44b3416d3dad936e5510c78e87b2212308
Ted Mader <ted.mader@ge.com>
2017-12-22 18:15:56 -0600

    Add sidenav specs

--- src/app/components/sidenav/sidenav.component.spec.ts
+++ src/app/components/sidenav/sidenav.component.spec.ts
@@ -34,4 +34,32 @@ describe('SidenavComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit a selectIndex event on click', () => {
+    spyOn(component.selectIndex, 'emit');
+    component.onClick(0);
+    expect(component.selectIndex.emit).toHaveBeenCalledWith(0);
+  });
+
+  it('should set the sidenav for small screens', () => {
+    spyOn(component.sidenav, 'close');
+    component.setForScreen(true);
+    expect(component.mode).toEqual('over');
+    expect(component.sidenav.disableClose).toBeFalsy();
+    expect(component.sidenav.close).toHaveBeenCalled();
+  });
+
+  it('should set the sidenav for large screens', () => {
+    spyOn(component.sidenav, 'open');
+    component.setForScreen(false);
+    expect(component.mode).toEqual('side');
+    expect(component.sidenav.disableClose).toBeTruthy();
+    expect(component.sidenav.open).toHaveBeenCalled();
+  });
+
+  it('should toggle the sidenav', () => {
+    spyOn(component.sidenav, 'toggle');
+    component.toggle();
+    expect(component.sidenav.toggle).toHaveBeenCalled();
+  });
 });
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,5 +1,5 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material';
 
 @Component({
@@ -12,17 +12,17 @@ export class SidenavComponent implements OnInit, OnDestroy {
   @Input() options: string[];
   @Output() selectIndex = new EventEmitter<number>();
   mobileQuery: MediaQueryList;
+  mode: string;
 
   private mobileQueryListener: () => void;
 
   constructor(media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this.mobileQueryListener = () => this.setForScreen();
-    this.mobileQuery.addListener(this.mobileQueryListener);
+    this.mobileQuery.addListener(() => this.setForScreen(this.mobileQuery.matches));
   }
 
   ngOnInit(): void {
-    this.setForScreen();
+    this.setForScreen(this.mobileQuery.matches);
   }
 
   ngOnDestroy(): void {
@@ -33,13 +33,15 @@ export class SidenavComponent implements OnInit, OnDestroy {
     this.selectIndex.emit(index);
   }
 
-  setForScreen(): void {
-    if (!this.mobileQuery.matches) {
-      this.sidenav.disableClose = true;
-      this.sidenav.open();
-    } else {
+  setForScreen(xs: boolean): void {
+    if (xs) {
+      this.mode = 'over';
       this.sidenav.disableClose = false;
       this.sidenav.close();
+    } else {
+      this.mode = 'side';
+      this.sidenav.disableClose = true;
+      this.sidenav.open();
     }
   }
 
