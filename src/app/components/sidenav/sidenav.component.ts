1230d852f9607bb4b455992b7662df5083e12e13
Ted Mader <tlmader.dev@gmail.com>
2017-12-27 20:40:47 -0600

    Add comments to sidenav logic

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,5 +1,5 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material';
 
 import { SidenavOptions } from '../../models/sidenav-options';
@@ -9,7 +9,7 @@ import { SidenavOptions } from '../../models/sidenav-options';
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent implements OnChanges, OnInit, OnDestroy {
+export class SidenavComponent implements OnChanges, OnDestroy {
   @ViewChild(MatSidenav) sidenav: MatSidenav;
   @Input() options: SidenavOptions[];
   @Output() selectIndex = new EventEmitter<number>();
@@ -23,6 +23,10 @@ export class SidenavComponent implements OnChanges, OnInit, OnDestroy {
     this.mobileQuery.addListener(() => this.setForScreen(this.mobileQuery.matches));
   }
 
+  /**
+   * Sets the sidenav on options changes.
+   * @param changes - contains options changes
+   */
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['options']) {
       if (changes['options'].currentValue) {
@@ -34,17 +38,22 @@ export class SidenavComponent implements OnChanges, OnInit, OnDestroy {
     }
   }
 
-  ngOnInit(): void {
-  }
-
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this.mobileQueryListener);
   }
 
+  /**
+   * Handles click events from an option by emitting the selectIndex event.
+   * @param index - the selected index.
+   */
   onClick(index: number): void {
     this.selectIndex.emit(index);
   }
 
+  /**
+   * Sets the sidenav for xs screen size or larger.
+   * @param xs - true if xs screen size
+   */
   setForScreen(xs: boolean): void {
     if (xs) {
       this.mode = 'over';
@@ -57,6 +66,9 @@ export class SidenavComponent implements OnChanges, OnInit, OnDestroy {
     }
   }
 
+  /**
+   * Toggles the sidenav.
+   */
   toggle(): void {
     this.sidenav.toggle();
   }
