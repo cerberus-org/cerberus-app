312308d9a33db1ae85907b3522356f91c9305546
Hillary Arurang <hillarurang@gmail.com>
2017-12-30 14:31:13 -0600

    Initialize action var in settings test

--- src/app/effects/settings.effects.spec.ts
+++ src/app/effects/settings.effects.spec.ts
@@ -2,6 +2,7 @@ import { async, TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Observable } from 'rxjs/Observable';
 
+import 'rxjs/add/observable/of';
 import { AuthService, MockAuthService } from '../services/auth.service';
 import { MockOrganizationService, OrganizationService } from '../services/organization.service';
 import { MockSnackBarService, SnackBarService } from '../services/snack-bar.service';
@@ -9,9 +10,10 @@ import { SettingsEffects } from './settings.effects';
 
 describe('SettingsEffects', () => {
   let effects: SettingsEffects;
-  const actions: Observable<any>;
+  let actions: Observable<any>;
 
   beforeEach(async(() => {
+    actions = Observable.of('');
     TestBed.configureTestingModule({
       providers: [
         SettingsEffects,
