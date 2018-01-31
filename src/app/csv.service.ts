2c31afec0323d69c7b71c45a51066002f3dffc95
Hillary Arurang <hillarurang@gmail.com>
2018-01-31 13:08:47 -0600

    Map visits to volunteers

--- src/app/csv.service.ts
+++ src/app/csv.service.ts
@@ -10,14 +10,37 @@ export class CsvService {
   constructor() { }
 
   mapVisitsToVolunteers(visits: Visit[], volunteers: Volunteer[]): any[] {
-    this.visitsWithNames = visits.filter(
-      visit => volunteers.filter(
-          volunteer => {
-            if (visit.id === volunteer.id) {
-              return  Object.assign({}, visit, { firstName: volunteer.firstName, lastName: volunteer.lastName });
-            }
-          }
-      ));
+    // this.visitsWithNames = visits.filter(
+    //   visit => volunteers.filter(
+    //       volunteer => {
+    //         if (visit.id === volunteer.id) {
+    //           return  Object.assign({}, visit, { firstName: volunteer.firstName, lastName: volunteer.lastName });
+    //         }
+    //       }
+    //   ));
+
+    // visits.forEach(function (visit) {
+    //   volunteers.forEach(function (volunteer) {
+    //     if (visit.volunteerId === volunteer.id) {
+    //       this.visitsWithNames.push(Object.assign({}, visit, { firstName: volunteer.firstName, lastName: volunteer.lastName }))
+    //     }
+    //   })
+    // });
+
+    this.visitsWithNames = [];
+    let found = false;
+    for (let i = 0, max = visits.length; i < max; i += 1) {
+      for (let j = 0, maxA = volunteers.length; j < maxA; j += 1) {
+        if (visits[i].volunteerId === volunteers[j].id) {
+          found = true;
+          this.visitsWithNames.push(Object.assign({}, visits[i], { firstName: volunteers[j].firstName, lastName: volunteers[j].lastName }))
+        }
+      }
+      if (!found) {
+        this.visitsWithNames.push(Object.assign({}, visits[i], { firstName: 'N/A', lastName: 'N/A' }));
+      }
+      found = false;
+    }
     return this.visitsWithNames;
   }
 }
