ce2307342535403cb9fcbb4c7241efe497cf90ff
Hillary Arurang <hillarurang@gmail.com>
2018-02-21 13:26:06 -0600

    Write test for transducer function

---
+++ src/app/functions/transducer.spec.ts
@@ -0,0 +1,23 @@
+import { testVisits } from '../models/visit';
+import { testVolunteers } from '../models/volunteer';
+import { formatDuration } from './date-format';
+import { mapVisitsToVolunteers } from './transducer';
+
+it('should format times', () => {
+  const visits = [testVisits[1]];
+  const volunteers = [testVolunteers[1]];
+  const expected = [{
+      id: '674a861ace7ca574af9070c1',
+      organizationId: '59a7055733bfe28af47cff40',
+      siteId: '59bc1e7ad92a6ac6f6252bfa',
+      volunteerId: '5961327dfba1ca1b64b8945b',
+      startedAt: new Date('2017-06-29T12:45:42.336Z'),
+      endedAt: new Date('2017-06-29T18:45:42.336Z'),
+      timezone: 'America/New_York',
+      signature: null,
+      name: volunteers[0].firstName + ' ' + volunteers[0].lastName,
+      duration: formatDuration(visits[0].startedAt, visits[0].endedAt, visits[0].timezone)
+  }];
+  const formatted = mapVisitsToVolunteers(visits, volunteers);
+  expect(formatted).toEqual(expected)
+});
