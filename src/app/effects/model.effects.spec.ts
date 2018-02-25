4500f804d9f6e6fd102449837ded961628fd79cc
Ted Mader <tlmader.dev@gmail.com>
2018-02-24 18:37:39 -0600

    Add specs for model effects

---
+++ src/app/effects/model.effects.spec.ts
@@ -0,0 +1,74 @@
+import { async, TestBed } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { cold, hot } from 'jasmine-marbles';
+import 'rxjs/add/observable/of';
+import { Observable } from 'rxjs/Observable';
+
+import * as ModelActions from '../actions/model.actions';
+import { filterByOrganizationId } from '../functions/helpers';
+import { testOrganizations } from '../models/organization';
+import { testSites } from '../models/site';
+import { testVisits } from '../models/visit';
+import { testVolunteers } from '../models/volunteer';
+import { mockServices } from './mock-services';
+import { ModelEffects } from './model.effects';
+import { SettingsEffects } from './settings.effects';
+
+describe('ModelEffects', () => {
+  let effects: SettingsEffects;
+  let actions: Observable<any>;
+  const organizationId: string = testOrganizations[0].id;
+
+  beforeEach(async(() => {
+    actions = Observable.of('');
+    TestBed.configureTestingModule({
+      providers: [
+        ModelEffects,
+        provideMockActions(() => actions),
+      ].concat(mockServices),
+    });
+    effects = TestBed.get(ModelEffects);
+  }));
+
+  describe('loadSites$', () => {
+    it('should dispatch AuthActions.LoadSitesSuccess', (() => {
+      actions = hot('a', {
+        a: new ModelActions.LoadSites(organizationId)
+      });
+      const expected = cold('b', {
+        b: new ModelActions.LoadSitesSuccess(
+          filterByOrganizationId(testSites, organizationId)
+        )
+      });
+      expect(effects.loadSites$).toBeObservable(expected);
+    }));
+  });
+
+  describe('loadVisits$', () => {
+    it('should dispatch AuthActions.LoadVisitsSuccess', (() => {
+      actions = hot('a', {
+        a: new ModelActions.LoadVisits(organizationId)
+      });
+      const expected = cold('b', {
+        b: new ModelActions.LoadVisitsSuccess(
+          filterByOrganizationId(testVisits, organizationId)
+        )
+      });
+      expect(effects.loadVisits$).toBeObservable(expected);
+    }));
+  });
+
+  describe('loadVolunteers$', () => {
+    it('should dispatch AuthActions.LoadVolunteersSuccess', (() => {
+      actions = hot('a', {
+        a: new ModelActions.LoadVolunteers(organizationId)
+      });
+      const expected = cold('b', {
+        b: new ModelActions.LoadVolunteersSuccess(
+          filterByOrganizationId(testVolunteers, organizationId)
+        )
+      });
+      expect(effects.loadVolunteers$).toBeObservable(expected);
+    }));
+  });
+});
--- src/app/functions/helpers.ts
+++ src/app/functions/helpers.ts
@@ -1,3 +1,9 @@
-export const sortVisitsByDate = visits => (
+import { Visit } from '../models/visit';
+
+export const sortVisitsByDate = (visits: Visit[]) => (
   visits.slice().sort((a, b) => b.startedAt.getTime() - a.startedAt.getTime())
+);
+
+export const filterByOrganizationId = (array: [], organizationId: string) => (
+  array.filter(item => item.organizationId === organizationId)
 );
