149a2c0ce498c6e5ff79171cc4e64db1358b3644
Hillary Arurang <hilllynn.dev@gmail.com>
2018-02-24 18:14:01 -0600

    Create join form

--- src/app/containers/join-page/join-page.component.ts
+++ src/app/containers/join-page/join-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from '../../models/user';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-join-page',
@@ -8,18 +9,42 @@ import {User} from '../../models/user';
 })
 export class JoinPageComponent implements OnInit {
 
-  userFormTitle: string;
-  validUser: User;
+  formGroup: FormGroup;
 
-  constructor() {
-    this.userFormTitle = 'Please enter your information.'
-  }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.formGroup = this.createForm();
   }
 
-  onValidUser(user: User) {
-    this.validUser = user;
+  createForm(): FormGroup {
+    return this.fb.group({
+      firstName: ['', [Validators.minLength(2), Validators.maxLength(35), Validators.required]],
+      lastName: ['', [Validators.minLength(2), Validators.maxLength(35), Validators.required]],
+      email: ['', [Validators.maxLength(255), Validators.required, Validators.email]],
+      password: ['', [Validators.minLength(8), Validators.maxLength(128), Validators.required]],
+      confirmPassword: ['', Validators.required],
+      organizationName: ['', Validators.required],
+    }, { validator: this.matchingPasswords('password', 'confirmPassword') });
+  }
+
+  /**
+   * Set confirmPassword control errors and form invalid if password and confirmPassword do not match.
+   * @param {string} passwordKey
+   * @param {string} confirmPasswordKey
+   * @returns {(group: FormGroup) => {[p: string]: any}}
+   */
+  matchingPasswords(passwordKey: string, confirmPasswordKey: string) {
+    return (group: FormGroup): {[key: string]: any} => {
+      const password = group.controls[passwordKey];
+      const confirmPassword = group.controls[confirmPasswordKey];
+
+      if (password.value !== confirmPassword.value) {
+        confirmPassword.setErrors({ mismatchedPasswords: true });
+        return { mismatchedPasswords: true };
+      }
+      confirmPassword.setErrors(null);
+    }
   }
 
   onJoinOrganization() {
