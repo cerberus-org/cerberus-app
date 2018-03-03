521d6577cd8bdf3847ec109316a7d9112734b906
Ted Mader <tlmader.dev@gmail.com>
2018-03-03 17:21:02 -0600

    Add users to model state

--- src/app/reducers/model.reducer.ts
+++ src/app/reducers/model.reducer.ts
@@ -1,15 +1,17 @@
 import * as ModelActions from '../actions/model.actions';
 import { sortVisitsByDate } from '../functions/helpers.functions';
-import { Site, Visit, Volunteer } from '../models';
+import { Site, User, Visit, Volunteer } from '../models';
 
 export interface State {
   sites: Site[];
+  users: User[];
   visits: Visit[];
   volunteers: Volunteer[];
 }
 
 export const initialState: State = {
   sites: [],
+  users: [],
   visits: [],
   volunteers: [],
 };
@@ -23,6 +25,12 @@ export function reducer(state = initialState, action: Action): State {
     case ModelActions.LOAD_SITES_SUCCESS: {
       return Object.assign({}, state, {
         sites: action.payload,
+      });
+    }
+
+    case ModelActions.LOAD_USERS_SUCCESS: {
+      return Object.assign({}, state, {
+        users: action.payload,
       });
     }
 
