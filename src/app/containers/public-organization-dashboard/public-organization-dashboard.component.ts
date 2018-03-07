bf0371ad0a70fc658c56d33044f98274735a366e
Hillary Arurang <hilllynn.dev@gmail.com>
2018-03-07 15:15:06 -0600

    Display public data

--- src/app/containers/public-organization-dashboard/public-organization-dashboard.component.ts
+++ src/app/containers/public-organization-dashboard/public-organization-dashboard.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as AppActions from '../../actions/app.actions';
+import { HeaderOptions } from '../../models';
+import { State } from '../../reducers';
 
 @Component({
   selector: 'app-public-organization-dashboard',
   templateUrl: './public-organization-dashboard.component.html',
-  styleUrls: ['./public-organization-dashboard.component.scss']
+  styleUrls: ['./public-organization-dashboard.component.scss'],
 })
 export class PublicOrganizationDashboardComponent implements OnInit {
 
-  constructor() { }
+  private headerOptions: HeaderOptions = new HeaderOptions(
+    'Public Dashboard',
+    'domain',
+    null,
+    false,
+  );
+
+  constructor(public store: Store<State>) { }
 
   ngOnInit() {
+    this.store.dispatch(new AppActions.SetHeaderOptions(this.headerOptions));
   }
-
 }
