2f50d1ac00ead57ad2fff4d14b56d650f446beba
Hillary Arurang <hillary.arurang@ge.com>
2018-07-24 16:02:53 -0500

    Write tests for visit selectors column options

--- src/app/settings/containers/visits/visits.component.spec.ts
+++ src/app/settings/containers/visits/visits.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockComponent } from 'ng2-mock-component';
+import {
+  convertToTimeString, formatDate, formatDuration, formatTime, formatTimeInputValue,
+  getFullName,
+} from '../../../functions';
 import { createMockVisits } from '../../../mock/objects/visit.mock';
 import { createMockVolunteers } from '../../../mock/objects/volunteer.mock';
 import { mockStoreModules } from '../../../mock/store-modules.mock';
+import { VisitWithVolunteer } from '../../../models/visit-with-volunteer';
 import * as SettingsActions from '../../store/actions/settings.actions';
 import { VisitsComponent } from './visits.component';
 
 describe('RolesComponent', () => {
   let component: VisitsComponent;
   let fixture: ComponentFixture<VisitsComponent>;
+  let visitWithVoluneer: VisitWithVolunteer;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -29,6 +35,10 @@ describe('RolesComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(VisitsComponent);
     component = fixture.componentInstance;
+    visitWithVoluneer = {
+      ...createMockVisits()[0],
+      volunteer: createMockVolunteers()[0],
+    };
     fixture.detectChanges();
   });
 
@@ -49,4 +59,42 @@ describe('RolesComponent', () => {
       );
     },
   );
+
+  it('should display the firstName and lastName of a volunteer in the first table column', () => {
+    expect(component.columnOptions[0].cell(visitWithVoluneer))
+      .toEqual(getFullName(visitWithVoluneer.volunteer));
+  });
+
+  it('should display formatted date in the second table column', () => {
+    expect(component.columnOptions[1].cell(visitWithVoluneer))
+      .toEqual(formatDate(visitWithVoluneer.startedAt, visitWithVoluneer.timezone));
+  });
+
+  it('should display formatted time for start date in the third table column', () => {
+    expect(component.columnOptions[2].cell(visitWithVoluneer))
+      .toEqual(formatTime(visitWithVoluneer.startedAt, visitWithVoluneer.timezone));
+  });
+
+  describe('endedAt column option', () => {
+
+    it('should display formatted time for input of type time for endedAt in the fourth table column', () => {
+      expect(component.columnOptions[3].cell(visitWithVoluneer))
+        .toEqual(formatTimeInputValue(visitWithVoluneer.endedAt, visitWithVoluneer.timezone));
+    });
+
+    it('should update visit with time when updateItemWithTime is called', () => {
+      expect(convertToTimeString(component.columnOptions[3].timePicker.updateItemWithTime('3:00', visitWithVoluneer).endedAt))
+        .toEqual('03:00');
+    });
+
+    it('should return false when validator is called and startedAt is after endedAt', () => {
+      expect(component.columnOptions[3].validator(visitWithVoluneer))
+        .toEqual(true);
+    });
+  });
+
+  it('should display duration of visit fifth table column', () => {
+    expect(component.columnOptions[4].cell(visitWithVoluneer))
+      .toEqual(formatDuration(visitWithVoluneer.startedAt, visitWithVoluneer.endedAt, visitWithVoluneer.timezone));
+  });
 });
