d615310b1c17707e9f742f709db50cd59f1b5a72
Hillary Arurang <hilllynn.dev@gmail.com>
2018-08-01 18:38:45 -0500

    Combine actions buttons and move to far right

--- src/app/shared/components/data-table/data-table.component.ts
+++ src/app/shared/components/data-table/data-table.component.ts
@@ -104,11 +104,8 @@ export class DataTableComponent implements OnInit, OnChanges {
     const cellPx = 49;
     this.initialPageSize = Math.floor((window.innerHeight - surroundingElementsPx) / cellPx);
     this.displayedColumns = this.columnOptions.map(column => column.columnDef);
-    if (this.showEdit) {
-      this.displayedColumns.push('edit');
-    }
-    if (this.showDelete) {
-      this.displayedColumns.push('delete');
+    if (this.showEdit || this.showDelete) {
+      this.displayedColumns.push('actions');
     }
   }
 
